12e090c03c12263c4d294125db66c250b0927fef
Kevin Huang <kevinhuangdreamer@gmail.com>
2014-05-24 21:12:04 +0800

    slide

--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,42 @@
 // popup.js
 $(function(){
-    console.log('popup');
+  console.log('popup');
     
-    $(".funnyPic").click(function(){
-        console.log("clicked");
-        var url = "www.blah.com/funnyPic" + $(this).attr("index");
-        $("#picUrl").val(url);
+  $(".funnyPic").click(function(){
+    console.log("clicked");
 
+    // change url
+    var url = "chrome-extension://obcibjhblhobgcdaaehocjokmahpppdd/images/image" 
+      + $(this).attr("index") 
+      + ".jpg";
+    $("#picUrl").val(url);
+
+    // navigate to preview
+    $(".previewPic").attr("src", url);
+    
+    $(".previewPic").load(function(){
+
+      var w = $(".previewPic").outerWidth();
+      if(w > 800){
+
+        w = 800;
+        $(".previewPic").css("width", 800);
+        
+      }
+      console.log("w: " + w);
+      $("html").animate({
+        width: w
+      }, 200, function(){});
+      $("body").animate({
+        width: w
+      }, 200, function(){});
+      $(".carousal").animate({
+        marginLeft: "-350px",
+        width: w+350
+      }, 200, function(){});
 
     });
+    
+
+  });
 });
